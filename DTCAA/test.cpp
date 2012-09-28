a978ae9ad315918cc1e95938f93c9e0bdf15005d
Asys <angelica_salazar@rocketmail.com>
2012-09-28 17:44:41 +0800

    Test

---
+++ DTCAA/test.cpp
@@ -0,0 +1,18 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Sep 28, 2012
+ *      Author: hp
+ */
+
+#include "test.h"
+
+test::test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+test::~test() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ DTCAA/test.h
@@ -0,0 +1,17 @@
+/*
+ * test.h
+ *
+ *  Created on: Sep 28, 2012
+ *      Author: hp
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class test {
+public:
+	test();
+	virtual ~test();
+};
+
+#endif /* TEST_H_ */
